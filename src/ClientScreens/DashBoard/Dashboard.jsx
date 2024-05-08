c5a9e60dc326f35e706d4253b8df4fe87d24e17e
Keshav Kokande <keshavkesu05@gmail.com>
2024-05-08 13:25:19 +0530

    changed backend links

--- src/ClientScreens/DashBoard/Dashboard.jsx
+++ src/ClientScreens/DashBoard/Dashboard.jsx
@@ -28,7 +28,7 @@ function DashboardCl() {
     window.scrollTo(0, 0);
     const fetchProfileData = async () => {
       try {
-        const response = await axios.get('http://localhost:8000/api/v1/Client/get-own-details', {
+        const response = await axios.get('https://team4api.azurewebsites.net/api/v1/Client/get-own-details', {
           headers: {
             'Content-Type': 'application/json'
           },
@@ -124,7 +124,7 @@ const formatDate = (dateString) => {
   useEffect(() => {
     const fetchTransactions = async () => {
       try {
-        const response = await fetch('http://localhost:8000/api/v1/Client/get-transactions', {
+        const response = await fetch('https://team4api.azurewebsites.net/api/v1/Client/get-transactions', {
           method: 'GET',
           headers: {
             'Content-Type': 'application/json'
@@ -154,7 +154,7 @@ const formatDate = (dateString) => {
   useEffect(() => {
     const fetchPlansData = async () => {
       try {
-        const response = await fetch('http://localhost:8000/api/v1/Client/get-all-plans', {
+        const response = await fetch('https://team4api.azurewebsites.net/api/v1/Client/get-all-plans', {
           method: 'GET',
           headers: {
             'Content-Type': 'application/json'
