d75b71fd750093b2aa4948b910bc43ddb254d3a1
Keshav Kokande <keshavkesu05@gmail.com>
2024-05-13 12:19:58 +0530

    changed ngrok link

--- src/components/dashboard/plans/PlanCard.jsx
+++ src/components/dashboard/plans/PlanCard.jsx
@@ -26,7 +26,7 @@ const PlanCard = ({ plan, deletePlan }) => {
           }))
         };
 
-        const response = await fetch('https://1fed-103-226-169-60.ngrok-free.app/calculate', {
+        const response = await fetch('https://15f4-103-226-169-60.ngrok-free.app/calculate', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json'
--- src/components/dashboard/plans/PlanEdit.jsx
+++ src/components/dashboard/plans/PlanEdit.jsx
@@ -82,7 +82,7 @@ const EditPlan = () => {
           }))
         };
 
-        const response = await fetch('https://1fed-103-226-169-60.ngrok-free.app/calculate', {
+        const response = await fetch('https://15f4-103-226-169-60.ngrok-free.app/calculate', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json'
