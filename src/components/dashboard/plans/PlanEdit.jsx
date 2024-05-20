f916c339741a35b5318842007257cac1012a7ee3
manran03 <92865525+manran03@users.noreply.github.com>
2024-05-20 09:19:47 +0530

    "stock-prices"

--- src/components/dashboard/plans/PlanEdit.jsx
+++ src/components/dashboard/plans/PlanEdit.jsx
@@ -55,8 +55,8 @@ const EditPlan = () => {
     };
     const fetchData = async () => {
       try {
-        const response = await axios.get('https://invest-nse.azurewebsites.net/get_symbol_lastprice');
-        setSelectedPrices(response.data);
+        const response = await axios.get('http://localhost:8000/api/v1/stock/get_symbol_lastprice');
+        setSelectedPrices(response.data.symbolPricesByDate);
         setDataLoading(1);
       } catch (error) {
         console.error('Error fetching data:', error);
