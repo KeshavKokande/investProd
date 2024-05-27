7bdb1ec7138bbc5672c3bc5b80a92ba05842cb50
Kashif Ahmad <91472410+Kashif-Ahmad-1@users.noreply.github.com>
2024-05-27 11:34:41 +0530

    changed redirect

--- src/components/SignIn/RedirectPage.jsx
+++ src/components/SignIn/RedirectPage.jsx
@@ -1,8 +1,9 @@
 import { useNavigate, useLocation } from 'react-router-dom';
 import { Box, Text, Center, Spinner } from '@chakra-ui/react';
-import './RedirectPage.css';
+// import './RedirectPage.css';
 import React, {useEffect} from 'react';
- 
+
+import styles from "./ThankYou.module.css";
 const RedirectPage = () => {
     const navigate = useNavigate();
     const location = useLocation();
@@ -41,51 +42,13 @@ const RedirectPage = () => {
     }, [location, navigate, fact, params]);
  
     return (
-<Box className="glossy-background">
-<Center height="100vh" flexDirection="column" p={[4, 6, 8]}>
-<Text
-                    mb="4"
-                    fontWeight="700"
-                    color="#475be8"
-                    fontSize={['40px', '60px', '100px']}
-                    fontFamily= 'Raleway, sans-serif'
-                    mt="20px"
->
-                    Welcome to INVEST
-</Text>
-<Text
-                    mb="4"
-                    color="#333333"
-                    fontSize={['20px', '30px', '50px']}
-                    fontFamily="sans-serif"
->
-                    Wait a while
-</Text>
-<Box display="flex" alignItems="center" mb="4">
-<Spinner size="xl" color="teal.500" mr="4" />
-<Text
-                        color="#333333"
-                        textAlign="center"
-                        fontFamily="sans-serif"
-                        fontSize={['16px', '20px', '24px']}
->
-                        Redirecting to the website
-</Text>
-</Box>
-                {fact && (
-<Box mt="4" bg="#6372ff" borderRadius="50px" p="4" mx="10" display="inline-block">
-<Text
-                        color="#F6F6F6"
-                        fontSize={['16px', '20px', '30px']}
-                        fontFamily="sans-serif"
-                        textAlign="center"
->
-                        {fact}
-</Text>
-</Box>
-                )}
-</Center>
-</Box>
+        <div className={styles.wrapper}>
+            <div className={styles.container}>
+                <h1 className={styles.heading}>Thank You for Registering!</h1>
+                <p className={styles.paragraph}>Your registration has been successfully completed.</p>
+                <a href="#" className={styles.button}>Go to Dashboard</a>
+            </div>
+        </div>
     );
 };
  
